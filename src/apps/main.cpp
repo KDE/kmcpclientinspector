56df49309f27583c07086a4aff2319279a9a50b9
Laurent Montel <montel@kde.org>
2026-05-02 07:14:24 +0200

    Prepare main.cpp

---
+++ src/apps/main.cpp
@@ -0,0 +1,18 @@
+﻿/*
+ * SPDX-FileCopyrightText: 2026 Laurent Montel <montel@kde.org>
+ *
+ * SPDX-License-Identifier: LGPL-2.0-or-later
+ */
+
+#include <KLocalizedString>
+#include <QApplication>
+using namespace Qt::Literals::StringLiterals;
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+
+    KLocalizedString::setApplicationDomain("kmcpclientinspector"_ba);
+
+    const int val = app.exec();
+    return val;
+}
