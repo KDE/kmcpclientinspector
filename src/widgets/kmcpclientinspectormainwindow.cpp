fa6cf9b3a3d019927d21da8c246d7c95eb2f0af7
Laurent Montel <montel@kde.org>
2026-05-02 10:16:52 +0200

    Add mainwindow

---
+++ src/widgets/kmcpclientinspectormainwindow.cpp
@@ -0,0 +1,14 @@
+/*
+ * SPDX-FileCopyrightText: 2026 Laurent Montel <montel@kde.org>
+ *
+ * SPDX-License-Identifier: LGPL-2.0-or-later
+ */
+
+#include "kmcpclientinspectormainwindow.h"
+
+KMcpClientInspectorMainWindow::KMcpClientInspectorMainWindow(QWidget *parent)
+    : KXmlGuiWindow(parent)
+{
+}
+
+KMcpClientInspectorMainWindow::~KMcpClientInspectorMainWindow() = default;
