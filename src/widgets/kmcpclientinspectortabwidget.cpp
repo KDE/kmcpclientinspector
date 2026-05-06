ce5feae426905dde4daa2f2c6153a13b6025a968
Laurent Montel <montel@kde.org>
2026-05-06 13:50:50 +0200

    Add tabwidget

---
+++ src/widgets/kmcpclientinspectortabwidget.cpp
@@ -0,0 +1,14 @@
+/*
+ * SPDX-FileCopyrightText: 2026 Laurent Montel <montel@kde.org>
+ *
+ * SPDX-License-Identifier: LGPL-2.0-or-later
+ */
+
+#include "kmcpclientinspectortabwidget.h"
+
+KMcpClientInspectorTabWidget::KMcpClientInspectorTabWidget(QWidget *parent)
+    : QTabWidget(parent)
+{
+}
+
+KMcpClientInspectorTabWidget::~KMcpClientInspectorTabWidget() = default;
---
+++ src/widgets/kmcpclientinspectortabwidget.h
@@ -0,0 +1,17 @@
+/*
+ * SPDX-FileCopyrightText: 2026 Laurent Montel <montel@kde.org>
+ *
+ * SPDX-License-Identifier: LGPL-2.0-or-later
+ */
+
+#pragma once
+#include "libkmcpclientinspectorwidgets_export.h"
+#include <QTabWidget>
+
+class LIBKMCPCLIENTINSPECTORWIDGETS_EXPORT KMcpClientInspectorTabWidget : public QTabWidget
+{
+    Q_OBJECT
+public:
+    explicit KMcpClientInspectorTabWidget(QWidget *parent = nullptr);
+    ~KMcpClientInspectorTabWidget() override;
+};
