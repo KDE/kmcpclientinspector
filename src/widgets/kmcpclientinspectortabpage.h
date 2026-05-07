9f3c85772a632650525e360bd2795f24458f64cf
Laurent Montel <montel@kde.org>
2026-05-07 13:33:43 +0200

    Add kmcpclientinspectortabpage

---
+++ src/widgets/kmcpclientinspectortabpage.h
@@ -0,0 +1,17 @@
+/*
+ * SPDX-FileCopyrightText: 2026 Laurent Montel <montel@kde.org>
+ *
+ * SPDX-License-Identifier: LGPL-2.0-or-later
+ */
+
+#pragma once
+#include "libkmcpclientinspectorwidgets_private_export.h"
+#include <QWidget>
+
+class LIBKMCPCLIENTINSPECTORWIDGETS_TESTS_EXPORT KMcpClientInspectorTabPage : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit KMcpClientInspectorTabPage(QWidget *parent = nullptr);
+    ~KMcpClientInspectorTabPage() override;
+};
