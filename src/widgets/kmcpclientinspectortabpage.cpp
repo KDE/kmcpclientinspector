011b03abb9a90b7a3011d421ec64657a859a29f2
Laurent Montel <montel@kde.org>
2026-05-09 21:19:33 +0200

    Add layout

--- src/widgets/kmcpclientinspectortabpage.cpp
+++ src/widgets/kmcpclientinspectortabpage.cpp
@@ -5,10 +5,23 @@
  */
 
 #include "kmcpclientinspectortabpage.h"
+#include <QSplitter>
+#include <QVBoxLayout>
 
+using namespace Qt::Literals::StringLiterals;
 KMcpClientInspectorTabPage::KMcpClientInspectorTabPage(QWidget *parent)
     : QWidget{parent}
 {
+    auto mainLayout = new QVBoxLayout(this);
+    mainLayout->setObjectName("mainLayout"_L1);
+    mainLayout->setContentsMargins({});
+    mainLayout->setSpacing(0);
+
+    // TODO
+
+    auto splitter = new QSplitter(this);
+    splitter->setObjectName(u"splitter"_s);
+    splitter->setChildrenCollapsible(false);
 }
 
 KMcpClientInspectorTabPage::~KMcpClientInspectorTabPage() = default;
