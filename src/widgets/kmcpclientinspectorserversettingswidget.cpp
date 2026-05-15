cb36792b7c6b4d808c02f99bdd2bcbffdc8c5f40
Laurent Montel <montel@kde.org>
2026-05-15 20:16:47 +0200

    Add layout

--- src/widgets/kmcpclientinspectorserversettingswidget.cpp
+++ src/widgets/kmcpclientinspectorserversettingswidget.cpp
@@ -5,10 +5,14 @@
  */
 
 #include "kmcpclientinspectorserversettingswidget.h"
-
+#include <QVBoxLayout>
+using namespace Qt::Literals::StringLiterals;
 KMcpClientInspectorServerSettingsWidget::KMcpClientInspectorServerSettingsWidget(QWidget *parent)
     : QWidget{parent}
 {
+    auto mainLayout = new QVBoxLayout(this);
+    mainLayout->setObjectName(u"mainLayout"_s);
+    mainLayout->setContentsMargins({});
 }
 
 KMcpClientInspectorServerSettingsWidget::~KMcpClientInspectorServerSettingsWidget() = default;
