c6a1d0674eaf303fc89d0da7bd6740fc19e511ed
Laurent Montel <montel@kde.org>
2026-05-17 11:29:50 +0200

    Prepare action widget

---
+++ src/widgets/actions/kmcpclientinspectorpingwidget.cpp
@@ -0,0 +1,18 @@
+/*
+ * SPDX-FileCopyrightText: 2026 Laurent Montel <montel@kde.org>
+ *
+ * SPDX-License-Identifier: LGPL-2.0-or-later
+ */
+
+#include "kmcpclientinspectorpingwidget.h"
+#include <QVBoxLayout>
+
+KMcpClientInspectorPingWidget::KMcpClientInspectorPingWidget(QWidget *parent)
+    : QWidget{parent}
+{
+    // TODO
+}
+
+KMcpClientInspectorPingWidget::~KMcpClientInspectorPingWidget() = default;
+
+#include "moc_kmcpclientinspectorpingwidget.cpp"
