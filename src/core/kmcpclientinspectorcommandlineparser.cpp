f00488bbfd751bebda3bf99d826721da89e42c78
Laurent Montel <montel@kde.org>
2026-05-20 08:58:07 +0200

    Prepare to add lineparser

---
+++ src/core/kmcpclientinspectorcommandlineparser.cpp
@@ -0,0 +1,33 @@
+﻿/*
+ * SPDX-FileCopyrightText: 2026 Laurent Montel <montel@kde.org>
+ *
+ * SPDX-License-Identifier: LGPL-2.0-or-later
+ */
+
+#include "kmcpclientinspectorcommandlineparser.h"
+#include <KLocalizedString>
+#include <QCommandLineParser>
+
+using namespace Qt::Literals::StringLiterals;
+KMcpClientInspectorCommandLineParser::KMcpClientInspectorCommandLineParser(QCommandLineParser *parser)
+{
+    initializeCommandLine(parser);
+}
+
+KMcpClientInspectorCommandLineParser::~KMcpClientInspectorCommandLineParser() = default;
+
+void KMcpClientInspectorCommandLineParser::initializeCommandLine(QCommandLineParser *parser)
+{
+    QCommandLineOption selfTestOption(optionParserFromEnum(OptionParser::SelfTest), QStringLiteral("internal, for automated testing"));
+    selfTestOption.setFlags(QCommandLineOption::HiddenFromHelp);
+    parser->addOption(selfTestOption);
+}
+
+QString KMcpClientInspectorCommandLineParser::optionParserFromEnum(OptionParser e)
+{
+    switch (e) {
+    case OptionParser::SelfTest:
+        return u"self-test"_s;
+    }
+    return {};
+}
---
+++ src/core/kmcpclientinspectorcommandlineparser.h
@@ -0,0 +1,23 @@
+﻿/*
+ * SPDX-FileCopyrightText: 2026 Laurent Montel <montel@kde.org>
+ *
+ * SPDX-License-Identifier: LGPL-2.0-or-later
+ */
+#pragma once
+#include "libkmcpclientinspectorcore_export.h"
+#include <QString>
+class QCommandLineParser;
+class LIBKMCPCLIENTINSPECTORCORE_EXPORT KMcpClientInspectorCommandLineParser
+{
+public:
+    enum class OptionParser : uint8_t {
+        SelfTest,
+    };
+    explicit KMcpClientInspectorCommandLineParser(QCommandLineParser *parser);
+    ~KMcpClientInspectorCommandLineParser();
+
+    [[nodiscard]] static QString optionParserFromEnum(OptionParser e);
+
+private:
+    LIBKMCPCLIENTINSPECTORCORE_NO_EXPORT void initializeCommandLine(QCommandLineParser *parser);
+};
